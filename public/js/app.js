fae2dc18e63afdebe505baec196a82104ace3f57
Aatif Mastan <aatifmastan644@gmail.com>
2022-03-16 20:56:16 +0530

    remove unnecessary code

--- public/js/app.js
+++ public/js/app.js
@@ -11,24 +11,6 @@
 
   let section = document.querySelector(".messages");
 
-  axios
-    .get("http://127.0.0.1:3000/get-messages")
-    .then((response) => {
-      let data = response.data;
-      console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        section.innerHTML += `
-          <div class="message">
-            <h3 class="name">${data[i].userName}</h3>
-            <h3 class="text">${data[i].message}</h3>
-          </div>
-        `;
-      }
-    })
-    .then((error) => {
-      if (error) throw error;
-    });
-
   sock.on("new_message", function (data) {
     console.log("Server says : " + data);
     section.innerHTML += `
